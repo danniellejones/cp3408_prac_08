ef300cd77721890d822bd63cba4a17cd162bf4a4
danniellejones <dannielle.jones1@my.jcu.edu.au>
2023-04-26 21:38:46 +1000

    Add fleeing action

--- CityCrowd/Assets/AIControl.cs
+++ CityCrowd/Assets/AIControl.cs
@@ -3,30 +3,63 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class AIControl : MonoBehaviour {
+public class AIControl : MonoBehaviour
+{
 
     GameObject[] goalLocations;
     NavMeshAgent agent;
     Animator anim;
+    float speedMult;
+    float detectionRadius = 20;
+    float fleeRadius = 10;
 
-    void Start() {
-
+    void Start()
+    {
         agent = GetComponent<NavMeshAgent>();
         goalLocations = GameObject.FindGameObjectsWithTag("goal");
         int i = Random.Range(0, goalLocations.Length);
         agent.SetDestination(goalLocations[i].transform.position);
         anim = this.GetComponent<Animator>();
-        anim.SetTrigger("isWalking");
         anim.SetFloat("wOffset", Random.Range(0.0f, 1.0f));
-        float sm = Random.Range(0.5f, 2);
-        anim.SetFloat("speedMult", sm);
-        agent.speed *= sm;
+        ResetAgent();
+    }
+
+    void ResetAgent()
+    {
+        speedMult = Random.Range(0.5f, 1.5f);
+        anim.SetFloat("speedMult", speedMult);
+        agent.speed *= speedMult;
+        anim.SetTrigger("isWalking");
+        agent.angularSpeed = 120;
+        agent.ResetPath();
+    }
+
+    public void DetectNewObstacle(Vector3 position)
+    {
+        if (Vector3.Distance(position, this.transform.position) < detectionRadius)
+        {
+            Vector3 fleeDirection = (this.transform.position - position).normalized;
+            Vector3 newGoal = this.transform.position + fleeDirection * fleeRadius;
+
+            NavMeshPath path = new NavMeshPath();
+            agent.CalculatePath(newGoal, path);
+
+            if (path.status != NavMeshPathStatus.PathInvalid)
+            {
+                agent.SetDestination(path.corners[path.corners.Length - 1]);
+                anim.SetTrigger("isRunning");
+                agent.speed = 10;
+                agent.angularSpeed = 500;
+            }
+        }
     }
 
 
-    void Update() {
+    void Update()
+    {
         if (agent.remainingDistance < 1)
         {
+            ResetAgent();
             int i = Random.Range(0, goalLocations.Length);
             agent.SetDestination(goalLocations[i].transform.position);
         }
